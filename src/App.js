03c1878094e65d6dfdf75d674a84e7af45320e27
Glayton <desenvolvimento1@ineosistemas.com.br>
2020-06-18 16:44:37 -0300

    Criado as rotas de listagem, cadastro e exclusão no front-end

--- src/App.js
+++ src/App.js
@@ -1,26 +1,64 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 import "./styles.css";
 
+import api from './services/api';
+
 function App() {
+
+  const [ repositories, setRepositories ] = useState([]);
+
+  useEffect( ()=> {
+    api.get('repositories').then(
+      response => {
+        setRepositories( response.data )
+      }
+    )
+  }, []);
+
   async function handleAddRepository() {
-    // TODO
+    
+    const repository = {
+      title : "Repositório NodeJS",
+      url : "http://github.com/glayton/node",
+      techs : [ "Node", "React"],
+    }
+
+    const response = await api.post('/repositories', repository );
+
+    if( response.status === 200 ){
+      setRepositories([...repositories, response.data]);
+    }
+
   }
 
-  async function handleRemoveRepository(id) {
-    // TODO
+  async function handleRemoveRepository( id ){
+    
+    const response = await api.delete(`/repositories/${id}`);
+
+    if( response.status === 204 ){
+      
+      const filteredRepositories = repositories.filter(repository => repository.id !== id);
+      setRepositories( filteredRepositories );
+
+    }
+
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+        { 
+          repositories.map( 
+            repository =>(
+              <li key={repository.id} >{repository.title}
+                <button onClick={() => handleRemoveRepository( repository.id )}>
+                  Remover
+                </button>
+              </li>
+            )
+          )
+        }
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
